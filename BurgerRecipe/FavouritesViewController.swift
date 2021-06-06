2b6de376300a713e3c3f18982fa77394c288acfe
Sherry1481 <shareyscorpi1@gmail.com>
2021-06-06 19:45:18 +0500

    commit dele files

---
+++ BurgerRecipe/FavouritesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavouritesViewController.swift
+//  BurgerRecipe
+//
+//  Created by sherry on 06/06/2021.
+//  Copyright © 2021 Gregor Pichler. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
